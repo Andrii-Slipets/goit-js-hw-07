23b81fc4d71f42438bd2b5570a772b85399df234
Andrii-Slipets <skodnik5740@gmail.com>
2024-01-23 20:46:36 +0200

    7

---
+++ js/task-3.js
@@ -0,0 +1,8 @@
+const nameInput = document.querySelector('#name-input');
+const nameOutput = document.querySelector('#name-output');
+nameInput.addEventListener("input", inputHandler);
+
+function inputHandler(event) {
+const trimmedValue = event.currentTarget.value.trim();
+  nameOutput.textContent = trimmedValue === "" ? "Anonymous" : trimmedValue;
+}
---
+++ js/task-5.js
@@ -0,0 +1,15 @@
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
+const changeColorBtn = document.querySelector('.change-color');
+const background = document.querySelector('body');
+const colorSpan = background.querySelector('.color')
+changeColorBtn.addEventListener("click", handleChangeColorBtn);
+
+
+function handleChangeColorBtn(event) {
+  background.style.backgroundColor = getRandomHexColor();
+  colorSpan.textContent = background.style.backgroundColor;
+}
